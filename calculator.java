ab72bd34184cc51f61b36f2f73ad9fd559e4aff0
JUNGSEUNGKIM <perghost5284@gmail.com>
2023-12-14 16:50:28 +0900

    ..

--- calculator.java
+++ calculator.java
@@ -1,1 +1,12 @@
 /추가1
+
+
+	int my_add(int number1, int number2){
+                        if (number1 + number2 < 0)
+                                return;
+                        return number1+number2;
+	}
+	
+
+// 추가 정승
+//
